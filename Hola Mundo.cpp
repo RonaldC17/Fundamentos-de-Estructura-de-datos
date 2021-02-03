edcf2e679d9e1202d3bbdea0d2e34c9ca9296850
RonaldC17 <78488599+RonaldC17@users.noreply.github.com>
2021-02-03 10:49:08 -0500

    Hora de la Comida
    EJEMPLO DE HORA DE LA COMIDA

--- Hola Mundo.cpp
+++ Hola Mundo.cpp
@@ -1,15 +1,38 @@
-#include "iostream"
+#include <iostream>
+#define desayuno_comienza 6
+#define desayuno_termina 9
+#define almuerzo_comienza 12
+#define almuerzo_termina 15
+#define cena_comienza 19
+#define cena_termina 21
 
 using namespace std;
+ 
+int main()
+{ 
+int horaactual;
+cout<<"que hora es: ";
+cin>>horaactual;
 
-int main{
+if((desayuno_comienza<=horaactual)&&(horaactual<=desayuno_termina))
+	{
+	cout<<" Es hora de desayunar";
+	}
+else if((almuerzo_comienza<=horaactual)&&(horaactual<=almuerzo_termina))
+	{
+	cout<<" Es hora de almorzar";
+	}
 
-    cout<<"Hola mundo";
-    return 0;
-
-
-
-
+else if((cena_comienza<=horaactual)&&(horaactual<=cena_termina))
+	{
+	cout<<" Es hora de cenar";
+	}
+else
+	{	
+	cout<<"No es hora de comer";
+	}
+return 0;
+    
 
 
 }
