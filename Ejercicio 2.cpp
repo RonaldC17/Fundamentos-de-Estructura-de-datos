54b202f5c4ef8bf0c64cb4da1512197702533b33
RonaldC17 <78488599+RonaldC17@users.noreply.github.com>
2021-02-03 16:11:55 -0500

    EJERCICIO DEL STRING
    Un ejercicio basico del string

---
+++ Ejercicio 2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string myString = "Hola a todos, bienvenidos a mi nuevo programa";
+  cout << myString[29];
+  return 0;
+}
+
---
+++ Ejercicio 4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string greeting = "kfmdfkjdfoaksmdnfslkghfdmflgnmgflbmcxlmxzñ{v,fok,xc{l ,mxpock}}";
+  std::cout << greeting;
+  return 0;
+}
+
