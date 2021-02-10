e3629ad9bdbd253989dce839090a82b539a68d09
RonaldC17 <78488599+RonaldC17@users.noreply.github.com>
2021-02-10 08:52:24 -0500

    EJERCICIOS EN CLASE 10 FEBRERO
    Ejercicios con el While y Do While

---
+++ EJERCICIO WHILE RECORDATORIO.cpp
@@ -0,0 +1,34 @@
+#include "iostream"
+
+using namespace std;
+
+int main(){
+	
+	int i=1;
+	
+    while(i<10000)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+    system("PAUSE");
+    return 0;
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
